71b14d79913f84a5df77c7bde0d5082aa7d16b36
RafidKhan <rafidkhan321.rk@gmail.com>
2021-02-18 14:51:58 +0600

    task 3 complete

--- app/src/main/java/com/example/task/Task3Activity.kt
+++ app/src/main/java/com/example/task/Task3Activity.kt
@@ -1,12 +1,39 @@
 package com.example.task
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.task.databinding.ActivityMainBinding
+import com.example.task.databinding.ActivityTask3Binding
 
 class Task3Activity : AppCompatActivity() {
+
+    lateinit var receiver: BroadcastReceiver
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val binding = ActivityTask3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                binding.actionTV.setText(intent?.action)
+                Toast.makeText(context, intent?.action, Toast.LENGTH_LONG).show()
+            }
+        }
+        registerReceiver(receiver, filter)
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 }
